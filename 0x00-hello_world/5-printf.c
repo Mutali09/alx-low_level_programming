cffee530bad3dfef6fc93e5b90da112155af99f5
Mutali09 <nelsonsimiyu9@gmail.com>
2023-07-14 16:09:51 -0700

    Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,10 @@
 /**
  * main - Entry point
  * Return: Always 0 (success)
+ *
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}
+}	
