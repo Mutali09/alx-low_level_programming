af7d3d36cf88f76279cece088aead1488e9f906a
Mutali09 <nelsonsimiyu9@gmail.com>
2023-07-16 23:33:44 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ *Description: prints exactly and that piece of art is useful"
+ * Return: Always 0 (Success)
+ */
 int main(void)
-	
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, 59);
-	return 1;
+	write(2, "and that pirce of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
