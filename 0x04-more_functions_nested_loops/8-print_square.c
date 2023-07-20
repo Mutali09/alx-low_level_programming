7ef22c3a5fd9c99c7fd29fbcabd24dfd018a4994
Mutali09 <nelsonsimiyu9@gmail.com>
2023-07-20 06:05:11 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,17 +10,17 @@ void print_square(int size)
 
 	if (size <= 0)
 	{
-		_putchar('\n');
+	_putchar('\n');
 	}
 	else
 	{
-		for (x = 0; x < size; x++)
-		{
-			for (y 0; y < size; y++)
-			{
-				_putchar(35);
-			}
-			putchar('\n');
-		}
+	for (x = 0; x < size; x++)
+	{
+	for (y = 0; y < size; y++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
 	}
 }
