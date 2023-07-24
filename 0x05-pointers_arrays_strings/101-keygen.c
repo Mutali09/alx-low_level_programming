747eb38fa5b5bae2708e35ec3b42bc2f83b3ea88
Mutali09 <nelsonsimiyu9@gmail.com>
2023-07-24 06:19:41 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -23,7 +23,7 @@ int main(void)
 		diff_half1 = (sum - 2772) / 2;
 		diff_half2 = (sum - 2772) / 2;
 		if ((sum - 2772) % 2 != 0)
-			diff_half1++;
+		diff_half1++;
 		for (index = 0; password[index]; index++)
 		{
 			if (password[index] >= (33 + diff_half1))
@@ -36,7 +36,7 @@ int main(void)
 		{
 			if (password[index] >= (33 + diff_half2))
 			{
-				password[index] -= diff-half2;
+				password[index] -= diff_half2;
 				break;
 			}
 		}
@@ -44,5 +44,3 @@ int main(void)
 	printf("%s", password);
 	return (0);
 }
-					
-
