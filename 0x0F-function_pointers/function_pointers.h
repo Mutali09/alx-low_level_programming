4d9cdc2162a8dcbab35e3126a29d5990fdc7b225
Mutali09 <nelsonsimiyu9@gmail.com>
2023-08-17 10:51:30 -0700

    3-main.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -8,15 +8,5 @@ void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-/**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
- */
-typedef struct op
-{
-    char *op;
-    int (*f)(int a, int b);
-} op_t;
+int (*get_op_func(char *s))(int, int);
 #endif
