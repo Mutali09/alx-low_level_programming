38fff9280568bb5dc787d7925633944d418b8a15
Mutali09 <nelsonsimiyu9@gmail.com>
2023-11-13 08:00:11 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * sub - subracts two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: subtracted final outcome
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
